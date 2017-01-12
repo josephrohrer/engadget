4dd3d0731fc329183dba448e01d95c3972edcb62
Joseph Rohrer <josephrohrer@me.com>
2017-01-12 09:47:18 -0700

    Created a page to write a persona

--- index.php
+++ index.php
@@ -6,26 +6,35 @@
 		<!-- load custom css here -->
 		<link href="css/style.css" rel="stylesheet" type="text/css" />
 
-		<title>Engadget Persona</title>
+		<title>Data Design Project - Engadget</title>
 	</head>
 
 	<main>
 		<header>
-			<h1>Persona</h1>
+			<h1>Data Design Project</h1>
+			<img src="images/logo.png" alt="Engadget logo">
+
 		</header>
+			<h2>User Persona</h2>
+				<p><strong>Name: </strong>dadlfkjadlfj<br>
+				<strong>Age: </strong>34</p>
 
-		<p><strong>Name: </strong>dadlfkjadlfj
-		<strong>Age: </strong>34</p>
+				<p><strong>Profession: </strong> is a walker on wall street</p>
 
-		<p><strong>Profession: </strong> is a walker on wall street</p>
+				<p><strong>Technology: </strong> uses a Macbook Pro</p>
 
-		<p><strong>Technology: </strong> uses a Macbook Pro</p>
+				<p><strong>Attitudes and Behaviors: </strong> is a dick</p>
 
-		<p><strong>Attitudes and Behaviors: </strong> is a dick</p>
+				<p><strong>Frustrations and Needs:</strong> is frustrated</p>
 
-		<p><strong>Frustrations and Needs:</strong> is frustrated</p>
+				<p><strong>Goals: </strong> wants to rule the world</p>
 
-		<p><strong>Goals: </strong> wants to rule the world</p>
+			<h2>Use Case</h2>
+
+
+			<h2>Interaction Flow</h2>
+
+			<h2>Conceptual Model</h2>
 		<!-- This is a comment -->
 	</main>
 
