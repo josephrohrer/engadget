1465463cd80e4dddf2589aaa2d6ad93a0829b406
Joseph Rohrer <josephrohrer@me.com>
2017-01-12 13:38:32 -0700

    Created a page to write a persona

--- index.php
+++ index.php
@@ -16,25 +16,40 @@
 
 		</header>
 			<h2>User Persona</h2>
-				<p><strong>Name: </strong>dadlfkjadlfj<br>
-				<strong>Age: </strong>34</p>
+				<ol>
+					<p><strong>Name: </strong>George Jetson<br>
+					<strong>Age: </strong>34</p>
 
-				<p><strong>Profession: </strong> is a walker on wall street</p>
+					<p><strong>Profession: </strong> George is a Product Development Manager for a company called "Big Dipper." Big Dipper is a start-up company whose first product is going to be building a robot named Rosie. Rosie is a robotic maid and housekeeper that will assist a family with everyday tasks including cooking, cleaning, and babysitting. </p>
 
-				<p><strong>Technology: </strong> uses a Macbook Pro</p>
+					<p><strong>Technology: </strong> Big Dipper provided George with a new 15" Macbook Pro (dongles in tow) updated to Mac OS Sierra and an iPhone 7 Plus with iOS 10.</p>
 
-				<p><strong>Attitudes and Behaviors: </strong> is a dick</p>
+					<p><strong>Attitudes and Behaviors: </strong> is a dick</p>
 
-				<p><strong>Frustrations and Needs:</strong> is frustrated</p>
+					<p><strong>Frustrations and Needs:</strong> is frustrated</p>
 
-				<p><strong>Goals: </strong> wants to rule the world</p>
+					<p><strong>Goals: </strong>  </p>
+				</ol>
 
 			<h2>Use Case</h2>
-
+				<ol>George has to stay up on the latest and greatest technology devices for his job, as well as for fun. He reads <a href="http://www.engadget.com">Engadget.com</a> regularly to keep up, as well as to research some of his competition.
+					<br>Rosie debuted at the 2017 Consumer Electronics Show and was featured by multiple news and media outlets. Engadget.com did a write-up on Rosie and George would like to see the initial reactions and comments that potential customers have on the site. He can also interact with potential customers and answer questions that they might have.
+				</ol>
 
 			<h2>Interaction Flow</h2>
+				<ol>
+
+
+				</ol>
 
 			<h2>Conceptual Model</h2>
+			<ul class="Conceptual-Model">
+				<strong>PROFILE</strong>
+				<p>profileId</p><br>
+				profileEmail
+				<
+
+			</ul>
 		<!-- This is a comment -->
 	</main>
 
