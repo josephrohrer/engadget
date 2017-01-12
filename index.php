672daafbb2050cc6e910e0e1d987abc23488f463
Joseph Rohrer <josephrohrer@me.com>
2017-01-12 08:56:09 -0700

    Created a page to write a persona

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 			<h1 class="foo-heading">This is a Demo page.</h1>
 		</header>
 		<p id="description"><b>Name: </b>dadlfkjadlfj</p>
-		<br>
 		<b>Age: </b>34
 		<br><br>
 		<b>Profession: </b> is a walker on wall street
@@ -22,50 +21,10 @@
 		<br><br>
 		<b>Attitudes and Behaviors: </b> is a dick
 		<br><br>
+		<b>Frustrations and Needs:</b> is frustrated
+		<br><br>
+		<b>Goals: </b> wants to rule the world
 		<!-- This is a comment -->
-
-		<h2>This is a header two.</h2>
-		<ul>
-			<li>Cats</li>
-			<li>Bowling</li>
-			<li>Tacos</li>
-			<li>Tow Trucks</li>
-		</ul>
-
-		<h3>This is a table with header three.</h3>
-		<table>
-			<!-- Row 1 -->
-			<tr>
-				<th>Column 1</th>
-				<th>Column 2</th>
-			</tr>
-
-			<!-- Row 2 -->
-			<tr>
-				<td>Tabby</td>
-				<td>Beagle</td>
-			</tr>
-
-			<!-- Row 3 -->
-			<tr>
-				<td>Persian</td>
-				<td>Bulldog</td>
-			</tr>
-
-			<!-- Row 4 -->
-			<tr>
-				<td>Siamese</td>
-				<td>Boxer</td>
-			</tr>
-		</table>
-
-		<p>This is a link to <a href="https://www.google.com/" target="_blank">Google</a> </p>
-
-		<div>This content is in a <div>.</div>
-			<span> This content is in a <span> </span>
-				<span> This content is in a <span></span>
-
-
 	</main>
 
 </html>
