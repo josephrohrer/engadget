d7612b08ae3c9a5dd39b1925e9236d84a8b6f228
Joseph Rohrer <josephrohrer@me.com>
2017-01-19 12:01:50 -0700

    Updated PHP file

--- php/classes/Comment.php
+++ php/classes/Comment.php
@@ -7,8 +7,6 @@
  * Time: 8:15 AM
  * This is my attempt of pretending like I know what I'm doing.
  */
-class Comment implements \JsonSerializable {
-	use ValidateDate;
 		/**
 		 * id for this Comment; this is the primary key
 		 * @var int $commentId
@@ -20,7 +18,7 @@ class Comment implements \JsonSerializable {
 		 **/
 		private $commentArticleId;
 		/**
-		 * id of the Profile that sent this Tweet; this is a foreign key
+		 * id of the Profile that sent this comment; this is a foreign key
 	 	 * @var int $commentProfileId;
 	 	 **/
 		private $commentProfileId;
@@ -34,15 +32,33 @@ class Comment implements \JsonSerializable {
 		 * @var string $commentContent
 		 **/
 		private $commentContent;
-	/**
-	 *
-	 * @var int $commentParentCommentId
-	 *
-	 **/
-		private $commentParentCommentId;
 
 
+		/**
+	 	* constructor for this Comment
 
+		 * @param int|null $newCommentId id of this comment or null if a new tweet
+		 * @param int $newCommentProfileId id of the Profile that created the comment
+		 * @param string $newCommentContent string containing actual comment data
+		 * @param \DateTime|string|null $newCommentDate date and time comment was made or null if set to current date and time
+		 * @throws \InvalidArgumentException if data types are not valid
+		 * @throws \RangeException if data values are out of bounds
+		 * @throws \TypeError if data types violate type hints
+		 * @throws \Exception if some other exception occurs
+		 **/
+		public function __construct(int $newCommentId = null, int $newCommentProfileId, string $newCommentContent, $newCommentDate = null) {
+			try {
+				$this->setCommentId($newCommentId);
+				$this->setCommentProfileId($newCommentProfileId);
+				$this->setCommentContent($newCommentContent);
+				$this->setCommentDate($newCommentDate);
+			} catch(\InvalidArgumentException $invalidArgument) {
+				// rethrow the exception to the caller
+				throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+			} catch
+
+
+	 */
 
 	}
 	}
