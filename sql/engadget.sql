24ac476e3dfc584aa8c64ec8b23da492720f432d
Joseph Rohrer <josephrohrer@me.com>
2017-01-19 10:41:59 -0700

    Updated SQL database

--- sql/engadget.sql
+++ sql/engadget.sql
@@ -9,7 +9,7 @@ CREATE TABLE profile (
 	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	profileEmail VARCHAR(128) NOT NULL,
 	profileHash CHAR(128) NOT NULL,
-	profileSalt char(64) NOT NULL,
+	profileSalt CHAR(64) NOT NULL,
 	UNIQUE(profileEmail),
 	PRIMARY KEY(profileId)
 
@@ -17,22 +17,25 @@ CREATE TABLE profile (
 
 
 CREATE TABLE article (
-	articleId,
+	articleId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	articleContent MEDIUMTEXT,
-	articleDate
+	articleDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	PRIMARY KEY(articleId)
 
 );
 
 
 
 CREATE TABLE comment (
-	commentId
-	commentArticleId
-	commentProfileId
-	commentDate
-	commentContent
-	commentParentId
-
+	commentId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	commentArticleId INT UNSIGNED NOT NULL,
+	commentProfileId INT UNSIGNED NOT NULL,
+	commentDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	commentContent CHAR(256),
+	INDEX(commentProfileId),
+	FOREIGN KEY(commentProfileId) REFERENCES profile(profileId),
+	FOREIGN KEY(commentArticleId) REFERENCES article(articleId),
+	PRIMARY KEY(commentId)
 
 );
 
